8d86e19c214836cf52577a622b63c1c17b9a4493
ddiazdom <ddiazdom@users.noreply.github.com>
2026-02-19 14:11:16 +0200

    add some compilation checks

--- cmake/CheckSIMDInstructions.cmake
+++ cmake/CheckSIMDInstructions.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXSourceCompiles)
 include(CheckCXXCompilerFlag)
 
-function(check_sse42_flags SIMD_FLAGS)
+function(check_sse42_flags FLAGS)
     set(CMAKE_CXX_FLAGS_BCK "${CMAKE_CXX_FLAGS}")
     set(CMAKE_CXX_FLAGS "-Wall -msse4.2")
     set(SSE42_CXX_CODE "
@@ -17,12 +17,23 @@ function(check_sse42_flags SIMD_FLAGS)
     )
     check_cxx_source_compiles("${SSE42_CXX_CODE}" SSE42_SUPPORT)
     if(SSE42_SUPPORT)
-        list(APPEND SIMD_FLAGS -msse4.2)
+        # Get the current parent-scope value (or empty if undefined)
+        if(DEFINED ${FLAGS})
+            set(current_flags "${${FLAGS}}")
+        else()
+            set(current_flags "")
+        endif()
+
+        # Append the new flag
+        list(APPEND current_flags -msse4.2)
+
+        # Write back to the parent scope
+        set(${FLAGS} "${current_flags}" PARENT_SCOPE)
     endif()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_BCK}")
 endfunction()
 
-function(check_avx2_flags SIMD_FLAGS)
+function(check_avx2_flags FLAGS)
     set(CMAKE_CXX_FLAGS_BCK "${CMAKE_CXX_FLAGS}")
     set(CMAKE_CXX_FLAGS "-Wall -mavx2")
     set(AVX2_CXX_CODE "
@@ -37,16 +48,37 @@ function(check_avx2_flags SIMD_FLAGS)
     )
     check_cxx_source_compiles("${AVX2_CXX_CODE}" AVX2_SUPPORT)
     if(AVX2_SUPPORT)
-        list(APPEND SIMD_FLAGS -mavx2)
+        # Get the current parent-scope value (or empty if undefined)
+        if(DEFINED ${FLAGS})
+            set(current_flags "${${FLAGS}}")
+        else()
+            set(current_flags "")
+        endif()
+
+        # Append the new flag
+        list(APPEND current_flags -mavx2)
+
+        # Write back to the parent scope
+        set(${FLAGS} "${current_flags}" PARENT_SCOPE)
     endif()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_BCK}")
 endfunction()
 
-function(check_bmi2_flags SIMD_FLAGS)
+function(check_bmi2_flags FLAGS)
     check_cxx_compiler_flag("-mbmi2" BMI2_SUPPORT)
     if(BMI2_SUPPORT)
-        list(APPEND SIMD_FLAGS -mbmi2)
-        message("Adding BMI2 as compilation flag")
+        # Get the current parent-scope value (or empty if undefined)
+        if(DEFINED ${FLAGS})
+            set(current_flags "${${FLAGS}}")
+        else()
+            set(current_flags "")
+        endif()
+
+        # Append the new flag
+        list(APPEND current_flags -mbmi2)
+
+        # Write back to the parent scope
+        set(${FLAGS} "${current_flags}" PARENT_SCOPE)
     endif()
 endfunction()
 
